d6aec08eeff5513243ae87f49f38e7fc3f3bbafe
Yosi <Yosi@YosiWebiks.com>
2024-11-10 11:48:47 +0200

    controller files and app.use listener

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,20 @@
-import express from 'express'
-import 'dotenv/config'
+import express from "express";
+import "dotenv/config";
+import usersController from "./controllers/users";
+import adminController from "./controllers/admin";
+import votesController from "./controllers/votes";
+import candidatesController from "./controllers/candidates";
 
-const PORT = process.env.PORT || 3000
-const app = express()
+const PORT = process.env.PORT || 3000;
+const app = express();
 
-app.listen(PORT, ()=>{
-    console.log(`Server started, Visit "http://localhost:${PORT}"`)
-})
+app.use(express.json());
+
+app.use("/api/users", usersController);
+app.use("/api/admin", adminController);
+app.use("/api/votes", votesController);
+app.use("/api/candidates", candidatesController);
+
+app.listen(PORT, () => {
+  console.log(`Server started, Visit "http://localhost:${PORT}"`);
+});
