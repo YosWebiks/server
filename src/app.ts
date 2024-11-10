fc4f6d6e25d3b548af9a90b894ab4be47ad70f12
Yosi <Yosi@YosiWebiks.com>
2024-11-10 12:38:32 +0200

    candidates sid

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,12 @@ import usersController from "./controllers/users";
 import adminController from "./controllers/admin";
 import votesController from "./controllers/votes";
 import candidatesController from "./controllers/candidates";
+import { connectToMongo } from "./config/db";
 
 const PORT = process.env.PORT || 3000;
+
 const app = express();
+connectToMongo()
 
 app.use(express.json());
 
--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,5 +1,10 @@
 import { Router } from "express";
+import { login, register } from "../routes/users";
 
 const router = Router()
+
+router.post('/login', login)
+
+router.post('/register', register)
 
 export default router
