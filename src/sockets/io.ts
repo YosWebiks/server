9a2b91abfb5c930c740c5534a8b6b835ffdcb732
Yosi <Yosi@YosiWebiks.com>
2024-11-12 17:02:36 +0200

    nofify client socket

--- src/sockets/io.ts
+++ src/sockets/io.ts
@@ -1,4 +1,5 @@
 import { Socket } from "socket.io";
+import { io } from "../app";
 
 export const handleSocketConnection = (client:Socket) => {
     console.log(`[socket.io] New Connection ${client.id}`)
@@ -8,5 +9,7 @@ export const handleSocketConnection = (client:Socket) => {
     client.on("newVote",()=>{
         console.log("[NEW VOTE] has accured in the system")
         // go tell all other connected clients to re-render their data
+        io.
+        emit("newDataHasOccurred")
     })
 };
